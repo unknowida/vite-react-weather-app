9b422458b0b830f0dc0a3feab1c1dc5b6f41761f
unknowida <csa.devomax@gmail.com>
2023-01-23 13:02:05 +0100

    Weather app in Vite+React JS

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import './App.css'
+import Search from './components/search/search'
+
+function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+
+
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/search/search.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+const Search = ({ onSearchChange }) => {
+
+    const [search, setSearch] = useState(null);
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+
+
+    }
+    return (
+       
+        <AsyncPaginate
+            placeholder="Rechercher votre ville"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+        />
+
+    )
+}
+
+export default Search;
